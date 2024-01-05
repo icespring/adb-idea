cf581b8a8dc753c91a025aa99da70d01f935ff53
yuanbingquan <yuanbingquan@baidu.com>
2024-01-05 16:07:01 +0800

    兼容Android Studio Hengehog

--- src/main/kotlin/com/developerphil/adbidea/adb/command/EnableImeCommand.kt
+++ src/main/kotlin/com/developerphil/adbidea/adb/command/EnableImeCommand.kt
@@ -9,16 +9,22 @@ import org.jetbrains.android.facet.AndroidFacet
 import java.util.concurrent.TimeUnit
 
 class EnableImeCommand : Command {
-    override fun run(project: Project, device: IDevice, facet: AndroidFacet, packageName: String): Boolean {
+    override fun run(context: CommandContext): Boolean {
         try {
-            if (AdbUtil.isAppInstalled(device, packageName)) {
-                device.executeShellCommand("ime enable $packageName/.ImeService", GenericReceiver(), 15L, TimeUnit.SECONDS)
+            if (AdbUtil.isAppInstalled(context.device, context.packageName)) {
+
+                context.device.executeShellCommand(
+                    "ime enable ${context.packageName}/.ImeService",
+                    GenericReceiver(),
+                    15L,
+                    TimeUnit.SECONDS
+                )
                 Thread.sleep(100)
-                device.executeShellCommand("ime set $packageName/.ImeService", GenericReceiver(), 15L, TimeUnit.SECONDS)
-                NotificationHelper.info(String.format("<b>%s</b> set ime for app on %s", packageName, device.name))
+                context.device.executeShellCommand("ime set ${context.packageName}/.ImeService", GenericReceiver(), 15L, TimeUnit.SECONDS)
+                NotificationHelper.info(String.format("<b>%s</b> set ime for app on %s", context.packageName, context.device.name))
                 return true
             } else {
-                NotificationHelper.error(String.format("<b>%s</b> is not installed on %s", packageName, device.name))
+                NotificationHelper.error(String.format("<b>%s</b> is not installed on %s", context.packageName, context.device.name))
             }
         } catch (e1: Exception) {
             NotificationHelper.error("Set Ime Enable failed... " + e1.message)
